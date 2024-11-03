5d5bf08f55530f4e9afb9f0afa81d7daefc53579
Tomasz1907 <147485043+Tomasz1907@users.noreply.github.com>
2024-11-03 17:29:54 +0100

    Update List.jsx

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,8 +7,8 @@ const value = "123";
   return (
     <div className='flex h-full lg:w-1/2 justify-self-center w-full bg-gray-500 border-[5px] p-5 text-white flex-col'>
         <div className='flex flex-row h-10 w-full'>
-            <input type="text" placeholder='wpisz zadanie' className='w-3/4 rounded-l-md p-3 text-black text-lg outline-none'/>
-            <button className='w-1/4 bg-slate-800 rounded-r-md hover:bg-slate-900'>Potwierdź</button>
+            <input type="text" placeholder='Type your task' className='w-3/4 rounded-l-md p-3 text-black text-lg outline-none'/>
+            <button className='w-1/4 bg-slate-800 rounded-r-md hover:bg-slate-900'>Confirm</button>
         </div>
         <ul className='bg-slate-600 h-full p-2 rounded-md flex flex-col gap-5 text-lg'>
             <li className='bg-slate-700 rounded-md flex flex-row justify-between gap-5 h-12 items-center px-2'>

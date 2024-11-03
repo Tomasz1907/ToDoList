7ca127433a469016be8130c2b0daa32502dc0b34
Tomasz1907 <147485043+Tomasz1907@users.noreply.github.com>
2024-11-03 17:27:08 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import List from "./components/List";
+
+const App = () =>{
+  return (
+    <main className="w-[100vw] h-[100vh] bg-gray-200">
+      <List></List>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/List.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faPenSquare, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+const List = () => {
+const value = "123";
+
+  return (
+    <div className='flex h-full lg:w-1/2 justify-self-center w-full bg-gray-500 border-[5px] p-5 text-white flex-col'>
+        <div className='flex flex-row h-10 w-full'>
+            <input type="text" placeholder='wpisz zadanie' className='w-3/4 rounded-l-md p-3 text-black text-lg outline-none'/>
+            <button className='w-1/4 bg-slate-800 rounded-r-md hover:bg-slate-900'>Potwierdź</button>
+        </div>
+        <ul className='bg-slate-600 h-full p-2 rounded-md flex flex-col gap-5 text-lg'>
+            <li className='bg-slate-700 rounded-md flex flex-row justify-between gap-5 h-12 items-center px-2'>
+            <p className='overflow-hidden'>{value}</p>
+            <div className='flex flex-row gap-2'>
+                <button
+                className='bg-slate-800 rounded-sm hover:bg-slate-900 p-1 w-10'>
+                  <FontAwesomeIcon icon={faCheck}/>
+                </button>
+                <button className='bg-slate-800 rounded-sm hover:bg-slate-900 p-1 w-10' >
+                  <FontAwesomeIcon icon={faPenSquare}/>
+                </button>
+                <button className='bg-slate-800 rounded-sm hover:bg-slate-900 p-1 w-10'>
+                  <FontAwesomeIcon icon={faXmark}/>
+                </button>
+            </div>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default List
